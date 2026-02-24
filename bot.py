db2beaff0915eb50e8219f3ee5d00db0d186bd08
olyabulavska-droid <olya.bulavska@gmail.com>
2026-02-24 11:37:26 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,38 @@
+import telebot
+import os
+
+TOKEN = os.getenv("TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+users = {}
+
+def get_points(uid):
+    return users.get(uid, 0)
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    kb = telebot.types.InlineKeyboardMarkup()
+    kb.add(telebot.types.InlineKeyboardButton("Мій баланс", callback_data="bal"))
+    kb.add(telebot.types.InlineKeyboardButton("Рейтинг", callback_data="rate"))
+    bot.send_message(msg.chat.id,"Меню",reply_markup=kb)
+
+@bot.callback_query_handler(func=lambda c: True)
+def cb(c):
+
+    if c.data=="bal":
+        p=get_points(c.from_user.id)
+        bot.answer_callback_query(c.id)
+        bot.send_message(c.message.chat.id,f"Баланс: {p}")
+
+    if c.data=="rate":
+        txt="🏆 Рейтинг\n\n"
+        sorted_users=sorted(users.items(),key=lambda x:x[1],reverse=True)
+        for i,(uid,p) in enumerate(sorted_users[:10]):
+            txt+=f"{i+1}. {uid} — {p}\n"
+        if not sorted_users:
+            txt="Поки пусто"
+        bot.answer_callback_query(c.id)
+        bot.send_message(c.message.chat.id,txt)
+
+bot.infinity_polling(
